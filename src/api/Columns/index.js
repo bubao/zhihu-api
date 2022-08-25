f38c107b37ab9e604cfdfa7e5bdae0143e59628d
bubao <asd565586630@gmail.com>
2022-08-25 11:30:36 +0800

    fix exports

--- src/api/Columns/index.js
+++ src/api/Columns/index.js
@@ -3,7 +3,7 @@
  * @Author: bubao
  * @Date: 2018-02-13 15:09:44
  * @LastEditors: bubao
- * @LastEditTime: 2022-08-25 11:14:20
+ * @LastEditTime: 2022-08-25 11:27:46
  */
 
 const Articles = require("./Articles");
@@ -11,10 +11,8 @@ const Followers = require("./Followers");
 const Items = require("./Items");
 const { info, coauthors } = require("./single");
 
-module.exports = {
-	info,
-	Articles,
-	Items,
-	Followers,
-	coauthors
-};
+exports.Articles = Articles;
+exports.info = info;
+exports.Items = Items;
+exports.Followers = Followers;
+exports.coauthors = coauthors;
